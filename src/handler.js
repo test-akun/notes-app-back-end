2af9e417bbe2f34e41e8a5e29df2854d2600ecf5
Ardato <codedokter@gmail.com>
2022-11-21 14:21:36 +0700

    part2 - get all notes handler & config routes.cors

--- src/handler.js
+++ src/handler.js
@@ -33,10 +33,14 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
-module.exports = {
-  addNoteHandler,
-};
+const getAllNotesHandler = () => ({
+  status: "success",
+  data: {
+    notes,
+  },
+});
 
 module.exports = {
   addNoteHandler,
+  getAllNotesHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
+const { addNoteHandler, getAllNotesHandler } = require("./handler");
+
 const routes = [
   {
     method: "POST",
     path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
   },
 ];
 
